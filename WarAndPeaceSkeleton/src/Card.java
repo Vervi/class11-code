125adacb5b18bafb4e92f89802ba2072eee62300
zhi <zhi@DESKTOP-16ISM72.fios-router.home>
2016-10-03 09:48:03 -0400

    winner method done

--- WarAndPeaceSkeleton/src/Card.java
+++ WarAndPeaceSkeleton/src/Card.java
@@ -15,9 +15,32 @@ public class Card {
 
 	public boolean winner(Card c) {
 			// this assumes that the game is running up town
-		
-		
-		
+		if ((this.value).compareTo(c.value)== 0)	// in case of a tie in numeric value
+		{
+			if ((this.suit).compareTo(c.suit)== 0) //nobody wins
+			{
+				return null;
+			}
+			if ((this.suit).compareTo(c.suit)> 0)
+			{
+				return true;
+			}
+			if ((this.suit).compareTo(c.suit) < 0)
+			{
+				return false;
+			}
+			
+		}
+		if ((this.value).compareTo(c.value) > 0)
+		{
+			return true;
+		}
+	
+		if ((this.value).compareTo(c.value) < 0)
+		{
+			return false;
+		}
+	
 		
 		
 	}
